b4ec23ab4635654a4e112a8c48238fa106a789f9
A R Shahrear <arshahrear30@gmail.com>
2026-01-10 14:40:59 +0600

    Update product_item.dart

--- widgets/product_item.dart
+++ widgets/product_item.dart
@@ -1,43 +1,89 @@
-class Product {
-  final String id;
-  final String productName;
-  final String productCode;
-  final String image;
-  final String unitPRice;
-  final String quantity;
-  final String totalPRice;
 
-  Product(
-      this.id,
-      this.productName,
-      this.productCode,
-      this.image,
-      this.unitPRice,
-      this.quantity,
-      this.totalPRice,
-      );
+import 'package:flutter/material.dart';
 
-  factory Product.fromJson(Map<String, dynamic> productJson) {
-    return Product(
-      productJson['_id'],
-      productJson['ProductName'] ?? '',
-      productJson['ProductCode']  ?? '',
-      productJson['Img']  ?? '',
-      productJson['UnitPrice']  ?? '',
-      productJson['Qty'] ?? '',
-      productJson['TotalPrice'] ?? '',
+import 'package:crud/screens/product.dart';
+import 'package:crud/screens/add_new_product_screen.dart';
+
+
+class ProductItem extends StatelessWidget {
+  const ProductItem(
+      {super.key, required this.product, required this.onPressDelete});
+
+  final Product product;
+  final Function(String) onPressDelete;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      onTap: () {
+        showDialog(
+            context: context,
+            builder: (context) {
+              return productActionDialog(context);
+            });
+      },
+
+
+
+
+      leading: Image.network(
+        product.image,
+        width: 80,
+      ),
+
+
+      title: Text(product.productName),
+      subtitle: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(product.productCode),
+          Text('Total price : ${product.totalPRice}'),
+          Text('Quantity : ${product.quantity}'),
+        ],
+      ),
+      trailing: Text('\$${product.unitPRice}'),
     );
   }
 
-  Map<String, dynamic> toJson() {
-    return {
-      '_id' : id,
-      'ProductName' : productName,
-      'ProductCode' : productCode,
-      'Img' : id,
-      'UnitPrice' : id,
-      'Qty' : id,
-      'TotalPrice' : id,
-    };
+  AlertDialog productActionDialog(BuildContext context) {
+    return AlertDialog(
+      title: const Text('Select action'),
+      content: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+
+          ListTile(
+            title: const Text('Edit'),
+            leading: const Icon(Icons.edit),
+            onTap: () {
+              Navigator.pop(context);
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => AddNewProductScreen(
+                    product: product,
+                  ),
+                ),
+              );
+            },
+          ),
+
+          const Divider(
+            height: 0,
+          ),
+
+          ListTile(
+            title: const Text('Delete'),
+            leading: const Icon(Icons.delete_outline),
+            onTap: () {
+              Navigator.pop(context);
+              onPressDelete(product.id);
+            },
+          ),
+
+
+        ],
+      ),
+    );
   }
 }
